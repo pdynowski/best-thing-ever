0c8ed9f642a784c37b86ae394ee0c24cd5cefc7d
Paul Dynowski <email": null>
2016-02-18 16:57:42 -0600

    Add elo update functionality to model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,25 @@ class Artist < ActiveRecord::Base
 
   has_many :winning_votes, foreign_key: :winner_id, class_name: 'Vote'
   has_many :losing_votes, foreign_key: :loser_id, class_name: 'Vote'
+
+
+  class << self
+    def assign_elo_points(artists)
+      k_factor = 32
+      winner_expectation = calc_expectation(artists)
+      loser_expectation = 1 - winner_expectation
+      artists[:winner].elo_score += (k_factor * (1 - winner_expectation)).to_i
+      artists[:loser].elo_score += (k_factor * (0 - loser_expectation)).to_i
+    end
+
+    private
+
+    def calc_expectation(artists)
+      winner_score = artists[:winner].elo_score
+      loser_score = artists[:loser].elo_score
+      expectation_denominator =
+        1 + 10**((loser_score - winner_score)/400)
+      return 1.0/expectation_denominator
+    end
+  end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -5,4 +5,20 @@ describe Artist do
   it {should validate_presence_of :elo_score }
   it {should have_many(:winning_votes).class_name("Vote")}
   it {should have_many(:losing_votes).class_name("Vote")}
+
+  describe "#assign_elo_points" do
+    let(:artist_1) { Artist.new(name: "xxx", elo_score: 1500)}
+    let(:artist_2) { Artist.new(name: "yyy", elo_score: 1500)}
+    it "should assign the correct number of points
+      to the winner" do
+      Artist.assign_elo_points({winner: artist_1, loser: artist_2})
+      expect(artist_1.elo_score).to eq 1516
+    end
+    it "should assign the correct number of points
+      to the loser" do
+      Artist.assign_elo_points({winner: artist_1, loser: artist_2})
+      expect(artist_2.elo_score).to eq 1484
+    end
+  end
+
 end
