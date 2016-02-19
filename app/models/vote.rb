c9dd1dc29a514e38561fb921e1b9834b688e9416
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-19 13:46:14 -0600

    Add first pass at #artist_score

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,31 @@ class Vote < ActiveRecord::Base
 
   belongs_to :winner, class_name: "Artist"
   belongs_to :loser, class_name: "Artist"
+
+  def self.artist_score
+    artist_hash = {}
+
+    Vote.all.each do |vote|
+      voted_artists = {
+        winner: vote.winner_id,
+        loser: vote.loser_id
+      }
+
+      if artist_hash.excludes?(voted_artists[:winner])
+        artist_hash << voted_artists[:winner] = 1500
+      end
+
+      if artist_hash.excludes?(voted_artists[:loser])
+        artist_hash << voted_artists[:loser] = 1500
+      end
+
+      artists_new_score = assign_elo_points(voted_artists)
+      artist_hash[voted_artists[:winner]] = artists_new_score[:winner]
+      artist_hash[voted_artists[:loser]] = artists_new_score[:loser]
+
+      return artist_hash
+    end
+
+  end
+
 end
