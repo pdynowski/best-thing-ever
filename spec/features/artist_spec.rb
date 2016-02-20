6bd88ef3f4bde9757bbbd26ba98f1b37fef96e73
Paul Dynowski <email": null>
2016-02-19 19:33:40 -0600

    Edit test file

--- spec/features/artist_spec.rb
+++ spec/features/artist_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'user visits artists#index' do
   scenario 'user should see a list of top artists' do
-    visit 'artists/index'
-    expect(page.find('.artist_score').length).to eq 40
+    visit 'artists/'
+    expect(page).to have_selector('.artist-row', count: 40)
   end
 end
