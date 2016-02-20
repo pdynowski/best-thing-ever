663ad7877a3e5c40dc2c28d3afc50170ab6d891b
Paul Dynowski <email": null>
2016-02-19 19:06:41 -0600

    Add top-ranked artist view page

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,16 @@
 class ArtistsController < ApplicationController
 
   def index
-    @artists_scores = Artist.score
-    puts @artists_scores
-    @artists = @artists_scores.sort_by do |key, value|
+    artists_scores = Artist.score
+    scores_sort = artists_scores.sort_by do |key, value|
       value
     end
-    puts @artists.reverse!
+    artists = scores_sort.reverse.shift(40)
+    @artists = artists.map do |artist|
+      artist_obj = Artist.find(artist[0])
+      [artist_obj.name, artist[1]]
+    end
+    @artists
   end
 
 end
