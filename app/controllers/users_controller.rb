f3905654677990a86c454c69dfb513ecd86c60f6
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-20 11:16:53 -0600

    Add a user_controller and user model and add necessary code.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+class UserController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:user_create_error] = user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def new
+  end
+
+  def show
+    @user = user.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      redirect_to user_path(user)
+    else
+      flash[:user_update_error] = user.errors.full_messages.to_sentence
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(session[:user_id])
+    if user.destroy
+      reset_session
+      flash[:user_successful_destroy] = "User account deleted."
+    else
+      flash[:user_error_destroy] = user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
+end
