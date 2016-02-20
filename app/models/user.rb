fa83f8549cd1cf4d156a10de539dd8e2db87dde0
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-20 13:05:05 -0600

    Update model to reflect user name and create user_controller_spec.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  validates :username, presence: true
   validates :email, presence: true, uniqueness: true
   has_secure_password
 
