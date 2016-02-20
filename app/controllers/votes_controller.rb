2e225a794ead51040def94e7dd22d9708d7d21f4
Paul Dynowski <email": null>
2016-02-20 11:39:42 -0600

    Retrieve previous vote info

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,13 @@
 class VotesController < ApplicationController
 
   def new
+    if flash[:prev_winner_id]
+      @prev_winner = Artist.find(flash[:prev_winner_id])
+      @prev_loser = Artist.find(flash[:prev_loser_id])
+      scores = Artist.score
+      @prev_winner_score = scores[@prev_winner.id]
+      @prev_loser_score = scores[@prev_loser.id]
+    end
     @option1 = Artist.get_random_artist
     @option2 = Artist.get_random_artist
     until @option1 != @option2
@@ -11,6 +18,8 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(winner_id: params[:winner], loser_id: params[:loser])
+    flash[:prev_winner_id] = params[:winner]
+    flash[:prev_loser_id] = params[:loser]
     if @vote.save
       redirect_to root_path
     else
