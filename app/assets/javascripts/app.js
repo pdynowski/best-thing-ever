57ae84eaeebd640d9a6cfb1575b1df9afe07c3f3
Caroline Kenworthy & Paul Dynowski <pair+kenworthyc.pdynowski@devbootcamp.com>
2016-02-20 21:11:17 -0600

    Do some freaky picture-in-picture stuff with the artist images

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
+
+
+   var url = $("#option1 img").attr('src');
+   $("#option1").css("background-image", "url("+url+")");
+   url = $("#option2 img").attr('src');
+   $("#option2").css("background-image", "url("+url+")");
+
   Mousetrap.bind('left', function(e) {
     $(".option1-vote").trigger("click");
   });
    Mousetrap.bind('right', function(e) {
     $(".option2-vote").trigger("click");
   });
+   Mousetrap.bind('down', function(e) {
+    location.reload(true);
+   });
+
+
 });
