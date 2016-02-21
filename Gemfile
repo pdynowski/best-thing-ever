8a90e3f2d5af856c681cf8ccf1dcae0e22210a7a
tommchenry <tommchenry@gmail.com>
2016-02-21 13:06:22 -0600

    Add user feature tests.

--- Gemfile
+++ Gemfile
@@ -24,13 +24,14 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'rails_12factor'
-gem 'figaro'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
+
+gem 'faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 require 'net/http'
 require 'json'
+require 'faker'
 
 (0..9).each do |count|
   url = "http://developer.echonest.com/api/v4/artist/search?api_key=#{Rails.application.secrets.echonest_api_key}&format=json&start=#{count * 100}&results=100&sort=familiarity-desc"
@@ -19,7 +20,12 @@ require 'json'
   end
 end
 
+User.create!(username: "test", email: "test@test.com", password: "test")
+
 (0..100).each do |num|
-  Vote.create!(winner_id: rand(1..500), loser_id: rand(501..1000))
+  Vote.create!(user_id: 1, winner_id: rand(1..500), loser_id: rand(501..1000))
 end
 
+(0..50).each do 
+  User.create!(username:Faker::Internet.user_name , email: Faker::Internet.email, password: "test")
+end
