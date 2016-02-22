dee8a84b86ec022ae8237133aa5ff677748ec5ba
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-22 13:34:30 -0600

    Remove small images from home

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,11 +1,5 @@
 $(document).ready(function() {
 
-
-   var url = $("#option1 img").attr('src');
-   $("#option1").css("background-image", "url("+url+")");
-   url = $("#option2 img").attr('src');
-   $("#option2").css("background-image", "url("+url+")");
-
   Mousetrap.bind('left', function(e) {
     $(".option1-vote").trigger("click");
   });
