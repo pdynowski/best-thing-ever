0f0c8abb6a26e64d71342c13be4ca8ac769f0398
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-24 15:32:46 -0600

    jQuery next-pair images, avg load .3 sec

--- app/assets/javascripts/preload.js
+++ app/assets/javascripts/preload.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+  var option1ImageUrl = $("#option1img").text();
+  var option2ImageUrl = $("#option2img").text();
+  $("#option1").css("background-image", "url("+ option1ImageUrl +")");
+  $("#option2").css("background-image", "url("+ option2ImageUrl +")");
 
+  var preloadImgUrl1 = $("#preload1img").text();
+  var preloadImgUrl2 = $("#preload2img").text();
+  setTimeout(function(){
+  $("#preload").append("<img src="+preloadImgUrl1+">");
+  $("#preload").append("<img src="+preloadImgUrl2+">");
+  }, 1000);
+
+});
