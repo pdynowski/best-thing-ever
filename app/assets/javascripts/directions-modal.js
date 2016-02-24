02f4d29e619db688c54ffc24a89b8ec6d3fda9ed
Paul Dynowski <email": null>
2016-02-24 12:31:14 -0600

    Add directions modal to mobile screen

--- app/assets/javascripts/directions-modal.js
+++ app/assets/javascripts/directions-modal.js
@@ -2,13 +2,20 @@ $(document).ready(function(){
 
   $('header').on("click", "#directions-modal-link", function(event) {
     event.preventDefault();
-    $('#modal-directions').css('display', 'block');
-  })
+    $('#desktop-modal-directions').css('display', 'block');
+  });
 
 
   $('.close').on("click", function(event) {
 
-    $('#modal-directions').css('display', 'none');
-  })
+    $('#desktop-modal-directions').css('display', 'none');
+    $('#mobile-modal-directions').css('display', 'none');
+  });
+
+  $('header').on("click", "#mobile-directions-modal-link", function(event) {
+    event.preventDefault();
+    console.log("clicky clicky")
+    $('#mobile-modal-directions').css('display', 'block');
+  });
 
 });
