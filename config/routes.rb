6e10c96f76155080f2d88e46ab600d2663c7179b
pdynowski <paul.dynowski@gmail.com>
2016-02-25 00:19:02 -0600

    Add create route for comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :entourages, only: [:new, :create]
   resources :developers, only: [:index]
   resources :about, only: [:index]
+  resources :comments, only [:create]
   root 'votes#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
