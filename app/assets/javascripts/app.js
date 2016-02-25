b649e9b3aeb31a1d51f2c2e0fe5bcd470f8411c9
Caroline Kenworthy & Tom McHenry <pair+kenworthyc.tommchenry@devbootcamp.com>
2016-02-25 14:22:50 -0600

    Add haptic feedback to images, up arrow to skip

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
   Mousetrap.bind('left', function(e) {
     $(".option1-vote").trigger("click");
   });
-   Mousetrap.bind('right', function(e) {
+  Mousetrap.bind('right', function(e) {
     $(".option2-vote").trigger("click");
   });
-   Mousetrap.bind('down', function(e) {
+  Mousetrap.bind('down', function(e) {
     location.reload(true);
-   });
-
+  });
+  Mousetrap.bind('up', function(e) {
+    location.reload(true);
+  });
 });
